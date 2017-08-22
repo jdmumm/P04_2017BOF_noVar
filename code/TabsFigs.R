3857b83fbcde4e461830ee1a0c76fd79b95786d9
Joshua Mumm <joshua.mumm@alaska.gov>
2017-08-22 15:44:24 -0800

    correct calculation of CPUE by area and stat

--- code/TabsFigs.R
+++ code/TabsFigs.R
@@ -3,35 +3,40 @@
 # This entire project is using the existing survey summary numbers (e.g. w/out variance)
 # All from the 2016 survey summary Spreadsheet created for KG Jan 2017.  These numbers don't exactly 
 # match the current query outputs in the DB.  However, they are close, w/in 1.1% surveywide for all years.
-# Going with the spreadsheet since those numbers should match those published elsewhere. 
+# Going with the spreadsheet since those numbers should match those published elsewhere, 
+# as requested by KG and JR.  
 
 ## Load ####
 library(tidyverse)
 library(reshape2)
 read.csv("data/surveyWide_from16SS.csv") -> surv  #Survey-wide summary by year
-read.csv("data/bySite_from16SS.csv") -> site      #by year and site
+read.csv("data/bySite_from16SS.csv")%>%
+  transmute(year = Year, site = Site_ID, pots = Pot_Count, all_cnt = Total_Spot_Count, all_lb = Total_Spot_Wt_KG * 2.20462, propLrg = Proportion_Large, 
+         lrg_cnt = Est_Count_LG, lrg_lb = Est_Wt_Large * 2.20462, cpue_all_lbs = CPUE_All_LB, cpue_all_cnt = CPUE_All_Count, cpue_lrg_cnt=CPUE_Large_Count) ->site
 read.csv("data/SiteStatArea_LUT.csv") -> siteStatLUT
 read.csv("data/femEggBySite.csv") -> egg
+read.csv("data/PWS Shrimp All.csv") %>%
+  select (year = DOL.Year, species = Species.Code, pots = Effort..sum., lbs = Whole.Weight..sum.) -> harv
 
 ## ASSEMBLE TABLES ON JRs LIST ####
 # CPUE (ALL_LB) by ShrimpArea and StatArea
   # join statArea and ShrimpArea to CPUE 
-    site %>% select(Year,SiteNum=Site_ID,CPUE_All_LB) %>%
+    site %>% select(year,site,pots,all_lb) %>%
     left_join (
-      siteStatLUT %>% select(-Comments)) %>%
-      filter(SiteNum != 11) -> cpueBySite         #exclude valdez 
+      siteStatLUT %>% select(-Comments), by = c("site" = "SiteNum")) %>%
+      filter(site != 11) -> cpueBySite         #exclude valdez 
   
   #by ShrimpArea
-    cpueBySite %>% group_by (Year,ShrimpArea) %>% summarize (
-      cpueAllLb = sum(CPUE_All_LB)) -> cpueByArea
-    dcast(cpueByArea, Year ~ ShrimpArea, value.var = "cpueAllLb") -> cpueByArea 
+    cpueBySite %>% group_by (year,ShrimpArea) %>% summarize (
+      cpueAllLb = sum(all_lb)/sum(pots)) -> cpueByArea
+    dcast(cpueByArea, year ~ ShrimpArea, value.var = "cpueAllLb") -> cpueByArea 
     write.csv(cpueByArea,"output/CPUE_allLb_byShrmpArea.csv")
     # get commercial data and join to above before writing 
  
   #by StatArea
-    cpueBySite %>% group_by (Year,StatArea) %>% summarize (
-      cpueAllLb = sum(CPUE_All_LB)) -> cpueByStat  
-    dcast(cpueByStat, Year ~ StatArea, value.var = "cpueAllLb" ) -> cpueByStat
+    cpueBySite %>% group_by (year,StatArea) %>% summarize (
+      cpueAllLb = sum(all_lb)/sum(pots)) -> cpueByStat  
+    dcast(cpueByStat, year ~ StatArea, value.var = "cpueAllLb" ) -> cpueByStat
     write.csv(cpueByStat,"output/CPUE_allLb_byStatArea.csv")
 
 # prop egg bearing by stat ####
@@ -69,7 +74,7 @@ read.csv("data/femEggBySite.csv") -> egg
   plot(cpueByArea[,1],cpueByArea[,4], main = "3", ylim = c(0,10))         
 
   par(mfrow = c(1,1), pch = 19, lwd = 2)  
-  plot(cpueByArea[,1],cpueByArea[,2], main = "CPUE_All_Lbs", ylim = c(0,12), type = "l", col = "red", 
+  plot(cpueByArea[,1],cpueByArea[,2], main = "CPUE_All_Lbs", ylim = c(0,4), type = "l", col = "red", 
         xlab = "year", ylab = "CPUE ALLs Lbs/pot")
   lines(cpueByArea[,1],cpueByArea[,3], col = "purple") 
   lines(cpueByArea[,1],cpueByArea[,4], col = "blue")
